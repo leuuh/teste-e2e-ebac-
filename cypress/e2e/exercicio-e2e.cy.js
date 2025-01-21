13de50477e9104cbe45d90fc87abe4831fb17b29
leuuh <leonardodealmeidagomes83@gmail.com>
2025-01-20 22:12:38 -0300

    salvando no git para feedback ebac

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import PaginaProducts from '../support/page_objects/PaginaProducts'
+import { faker } from '@faker-js/faker';
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
@@ -10,13 +12,57 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+      PaginaProducts.visitUrl()
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
+    cy.fixture('massadados').then(dados => {
+
+        PaginaProducts.buscarProdutos(dados[0].nomeProduto);
+        PaginaProducts.addProdutoCarrinho(
+            dados[0].tamanho,
+            dados[0].cor,
+            dados[0].quantidade)
+    cy.get('.woocommerce-message', { timeout: 10000 }).should('contain', dados[0].nomeProduto);
+
+    });
+
+    cy.fixture('massadados').then(dados => {
+
+      PaginaProducts.buscarProdutos(dados[1].nomeProduto);
+      PaginaProducts.addProdutoCarrinho(
+          dados[1].tamanho,
+          dados[1].cor,
+          dados[1].quantidade)
+    cy.get('.woocommerce-message', { timeout: 10000 }).should('contain', dados[1].nomeProduto)
+    });  
+
+    cy.fixture('massadados').then(dados => {
+
+      PaginaProducts.buscarProdutos(dados[2].nomeProduto);
+      PaginaProducts.addProdutoCarrinho(
+          dados[2].tamanho,
+          dados[2].cor,
+          dados[2].quantidade)
+    cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)      
+    });  
+
+    cy.fixture('massadados').then(dados => {
+
+      PaginaProducts.buscarProdutos(dados[3].nomeProduto);
+      PaginaProducts.addProdutoCarrinho(
+          dados[3].tamanho,
+          dados[3].cor,
+          dados[3].quantidade)
+    cy.get('.woocommerce-message').should('contain', dados[3].nomeProduto)
+    });
+
+    cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+    cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+    cy.get('#billing_first_name')
+
+    it('variaveis', () => {
       
+    });
   });
-
-
-})
+});
