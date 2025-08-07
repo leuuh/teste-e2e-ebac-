b84bfa5c2ba1048aa5010b73b2720cf92e0c3383
Leonardo de Almeida <leonardodealmeidagomes83@gmail.com>
2025-08-07 18:29:23 -0300

    ci em pipeline

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,8 @@
-//Cypress.Commands.add('login', (usuario, senha) => {
-   //cy.get('#username').type(usuario)
-    //cy.get('#password').type(senha, {log: false})
-    //cy.get('.woocommerce-form > .button').click()
-//});
+Cypress.Commands.add('login', (usuario, senha) => {
+   cy.get('#username').type(usuario)
+    cy.get('#password').type(senha, {log: false})
+    cy.get('.woocommerce-form > .button').click()
+});
 
 Cypress.Commands.add('selecionarTamanho', (tamanho) => {
   cy.get(`.button-variable-item-${tamanho}`).click({ timeout: 10000 });
